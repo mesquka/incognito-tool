d19689fc565fe807418e242bf3e7b519641f0bd6
Kieran Mesquita <mesquka@pm.me>
2020-02-01 04:30:48 +0800

    About

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,6 +10,10 @@
         This is an <i>unofficial</i> web app to help you keep tabs on your incognito nodes.
         If you want to start staking yourself you can get started <a href="https://incognito.org/">here</a>.
       </div>
+      <footer class="card-footer">
+        <a href="https://incognito.org/node.html" class="card-footer-item">Start Staking (Easy)</a>
+        <a href="https://incognito.org/validator.html" class="card-footer-item">Start Staking (Advanced)</a>
+      </footer>
     </div>
 
     <div class="spacer"></div>
@@ -24,6 +28,9 @@
         If you're having issues, open a github issue <a href="https://github.com/mesquka/incognito_mining_dashboard/issues/new">here</a>,
         keep in mind this is a side project, so I'm not likely to dedicate much time to it.
       </div>
+      <footer class="card-footer">
+        <a href="https://github.com/mesquka/incognito_mining_dashboard/issues/new" class="card-footer-item">New Github Issue</a>
+      </footer>
     </div>
 
     <div class="spacer"></div>
@@ -37,6 +44,9 @@
       <div class="card-content">
         If you found this app useful, consider dropping me a few cryptos <a href="https://mesquka.com/tips">here</a>.
       </div>
+      <footer class="card-footer">
+        <a href="https://mesquka.com/tips" class="card-footer-item">Tip Me</a>
+      </footer>
     </div>
   </div>
 </template>
