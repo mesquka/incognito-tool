d4ad7aaa5322e49d4bdaf5ea83959b76c14fe6e1
Kieran Mesquita <mesquka@pm.me>
2020-02-11 04:48:43 +0800

    Shards and Pending Transactions in Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -15,9 +15,6 @@
           <div class="content">
             <p class="title">{{blockchain.TotalTxs}}</p>
             <p class="subtitle">Transactions Processed</p>
-            <div class="content">
-              <!-- Content -->
-            </div>
           </div>
         </article>
       </div>
@@ -27,9 +24,6 @@
           <div class="content">
             <p class="title">{{mempool.Size}}</p>
             <p class="subtitle">Pending Transactions</p>
-            <div class="content">
-              <!-- Content -->
-            </div>
           </div>
         </article>
       </div>
@@ -39,11 +33,84 @@
           <div class="content">
             <p class="title">{{blockchain.Beacon.Epoch}}</p>
             <p class="subtitle">Epoch</p>
-            <div class="content">
-              <!-- Content -->
-            </div>
           </div>
         </article>
+      </div>
+    </div>
+
+    <div class="spacer"></div>
+
+    <div class="columns">
+      <div class="column">
+        <div>
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                Beacon Chain
+              </p>
+            </header>
+            <div class="card-content">
+              <table class="table is-bordered is-hoverable is-fullwidth">
+                <tbody>
+                  <tr>
+                    <th>Latest Block</th>
+                    <td>{{blockchain.Beacon.Hash}}</td>
+                  </tr>
+                  <tr>
+                    <th>Height</th>
+                    <td>{{blockchain.Beacon.Height}}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="spacer"></div>
+        </div>
+
+        <div v-for="(shard, index) in blockchain.Shards" v-bind:key="index">
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                Shard #{{shard.id}}
+              </p>
+            </header>
+            <div class="card-content">
+              <table class="table is-bordered is-hoverable is-fullwidth">
+                <tbody>
+                  <tr>
+                    <th>Latest Block</th>
+                    <td>{{shard.Hash}}</td>
+                  </tr>
+                  <tr>
+                    <th>Height</th>
+                    <td>{{shard.Height}}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="spacer"></div>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title">
+              Pending Transactions
+            </p>
+          </header>
+          <div class="card-content">
+            <span v-if="mempool.ListTxs.lenght === 0">No Pending Transactions</span>
+            <table class="table is-bordered is-hoverable is-fullwidth">
+              <tbody>
+                <tr v-for="(tx, index) in mempool.ListTxs" v-bind:key="index">
+                  <td>{{tx.TxID}}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -72,5 +139,9 @@ export default {
 
 .table {
   table-layout: fixed;
+}
+
+td {
+  overflow: auto;
 }
 </style>
